6ff5c793607cb09cd389900fffb6414d654fee6f
Weronika Kansy <weronika.kansy@gmail.com>
2022-03-09 23:08:53 +0100

    Props passed into the Card component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import { Navbar } from "./components/Navbar";
 import { Hero } from "./components/Hero";
+import { Card } from "./components/Card";
+import photo12 from "./assets/image_12.png";
 
 function App() {
   return (
     <div className="container">
       <Navbar />
       <Hero />
+      <Card
+        img={photo12}
+        rating="5.0"
+        reviewCount={6}
+        country="USA"
+        title="Life lessons with Katie Zaferes"
+        price={135}
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,21 @@
-import photo from "../assets/image_12.png";
 import star from "../assets/star.svg";
 
-export function Card() {
+export function Card({ img, rating, reviewCount, country, title, price }) {
   return (
     <div className="card">
       <div className="card__img-wrapper">
-        <img src={photo} alt="" className="card__img" />
+        <img src={img} alt="" className="card__img" />
         <p className="card__badge">Sold out</p>
       </div>
       <div className="card__stats">
         <img src={star} alt="Star" />
-        <span className="card__rating">5.0</span>
-        <span className="card__count">(6) ·</span>
-        <span className="card__state">USA</span>
+        <span className="card__rating">{rating}</span>
+        <span className="card__count">({reviewCount}) ·</span>
+        <span className="card__state">{country}</span>
       </div>
-      <p className="card__title">Life lessons with Katie Zaferes</p>
+      <p className="card__title">{title}</p>
       <p className="card__price">
-        <strong>From $136</strong> / person
+        <strong>From ${price}</strong> / person
       </p>
     </div>
   );
